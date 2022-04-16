2c41ee8d88c71cb47bfed7fd2cb2ef8b44f16e07
Ramesh Bheemappa <ramesh_bheemappa@cable.comcast.com>
2022-04-16 12:16:19 -0600

    release-1

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="main-container">
+    <header></header>
+    <router-view></router-view>
+    <footer></footer>
+  </div>
+  <div class="page">
+    <router-view></router-view>
+    <div class="help-text">
+      <p>viewing my repositories goes here.</p>
+      <a href="https://github.com/ramesb/SE577-week-1">View Source Code</a>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
---
+++ src/App1.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="app">
+  <div class="header">
+  <header-bar>SE577 SPA </header-bar>
+  </div>
+  <div class="page">
+    <router-view></router-view>
+    <div class="help-text">
+      <p>viewing my repositories goes here.</p>
+      <a href="https://github.com/ramesb/SE577-week-1">View Source Code</a>
+    </div>
+  </div>
+</div>
+</template>
